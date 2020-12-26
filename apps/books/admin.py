1e7121eaba29e806c94df094824e3a1a4b4fa55b
oguzhanoxel <oguzhanoksel@hotmail.com>
2020-12-26 11:38:15 +0300

    first commit: added books app

---
+++ apps/books/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'title', 'author', 'pages',
+    )
+    list_display_links = (
+        'title', 'author', 'pages',
+    )
+
+
+admin.site.register(Book)
